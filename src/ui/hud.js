fa1ce509ea6150af1d99caa99c553b365a6a5e78
mamanggim <mamanggim@gmail.com>
2025-08-26 11:17:07 +0700

    Update hud.js

--- src/ui/hud.js
+++ src/ui/hud.js
@@ -1,1 +1,8 @@
-
+// [ID-UI-HUD-V1]
+export function renderHUD(player) {
+  const hud = document.getElementById("hud");
+  hud.innerHTML = `
+    <p>Player: ${player.name}</p>
+    <p>Monster pertama: ${player.monsters?.[0]?.name || "Belum ada"}</p>
+  `;
+}
