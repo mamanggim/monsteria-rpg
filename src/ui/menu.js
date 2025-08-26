5c7a2e18e659f6321c276565e1d8e2eefd64d457
mamanggim <mamanggim@gmail.com>
2025-08-26 12:34:23 +0700

    Update menu.js

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -1,1 +1,15 @@
+import { explore } from "../core/explore.js";
+import { log } from "./log.js";
 
+export function initMenu() {
+  const menuDiv = document.getElementById("menu");
+  menuDiv.innerHTML = `
+    <button id="exploreBtn">Explore</button>
+    <button id="monstersBtn">My Monsters</button>
+  `;
+
+  document.getElementById("exploreBtn").onclick = explore;
+  document.getElementById("monstersBtn").onclick = () => {
+    log("Your monsters: " + window.game.player.monsters.map(m => m.name).join(", ") || "None");
+  };
+}
